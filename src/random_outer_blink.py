03f968ce2b84d38c212b769c5a03afaff8c7bab9
Balazs Marton <balazs.marton@prezi.com>
2021-12-22 14:06:42 +0100

    feat: add script to light up random outer leds

---
+++ src/random_outer_blink.py
@@ -0,0 +1,11 @@
+from star import Star
+from random import randint
+
+star = Star(pwm=True)
+leds = star.leds
+try:
+    while True:
+        led = star.leds[randint(1, len(leds) - 1)]
+        led.pulse(fade_in_time=1, fade_out_time=1, n=1, background=False)
+except KeyboardInterrupt:
+    star.close()
