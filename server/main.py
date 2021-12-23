09e103e3f44ec70a1500835bb8ce5935680896b5
Balazs Marton <balazs.marton@prezi.com>
2021-12-23 20:53:16 +0100

    fix: remove trailing slashes for clearer urls

--- server/main.py
+++ server/main.py
@@ -6,11 +6,11 @@ app = FastAPI()
 
 star_controller = StarController()
 
-@app.post("/loop/{snippet_name}/")
+@app.post("/loop/{snippet_name}")
 def loop_snippet(snippet_name: str):
     snippet = getattr(snippets, snippet_name)
     star_controller.start_loop(snippet)
 
-@app.post("/stop/")
+@app.post("/stop")
 def stop():
     star_controller.stop()
