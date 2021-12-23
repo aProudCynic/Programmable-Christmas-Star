b04ecbc1af73554c99db42a3e96478df7d5a6c91
Balazs Marton <balazs.marton@prezi.com>
2021-12-23 20:24:52 +0100

    chore: clearer endpoint url

--- server/main.py
+++ server/main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 star_controller = StarController()
 
-@app.get("/{snippet_name}/")
+@app.get("/loop/{snippet_name}/")
 def loop_snippet(snippet_name: str):
     snippet = getattr(snippets, snippet_name)
     star_controller.start_loop(snippet)
