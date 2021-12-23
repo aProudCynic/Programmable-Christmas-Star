c12c64f679c3af876216ba4f871c81d9bef5a3d0
Balazs Marton <balazs.marton@prezi.com>
2021-12-23 21:21:21 +0100

    refact: clearer naming

--- server/main.py
+++ server/main.py
@@ -1,22 +1,22 @@
 from fastapi import FastAPI
 from controller.star_controller import StarController
-import controller.snippets as snippets
+import controller.light_programmes as light_programmes
 from inspect import getmembers, isfunction
 
 app = FastAPI()
 
 star_controller = StarController()
 
-@app.post("/loop/{snippet_name}")
-def loop_snippet(snippet_name: str):
-    snippet = getattr(snippets, snippet_name)
+@app.post("/loop/{light_programme_name}")
+def loop_snippet(light_programme_name: str):
+    snippet = getattr(light_programmes, light_programme_name)
     star_controller.start_loop(snippet)
 
 @app.post("/stop")
 def stop():
     star_controller.stop()
 
-@app.get("/snippets")
+@app.get("/light_programmes")
 def get_snippets():
-    function_tuples = getmembers(snippets, isfunction)
+    function_tuples = getmembers(light_programmes, isfunction)
     return [function_tuple[0] for function_tuple in function_tuples]
