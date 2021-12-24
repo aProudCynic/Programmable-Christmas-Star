605c3a40d2c331d388b07255b1d9a847e088a3cc
Balazs Marton <balazs.marton@prezi.com>
2021-12-24 21:27:22 +0100

    feat: implement select

--- client/src/select/Select.tsx
+++ client/src/select/Select.tsx
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { SERVER_URL } from '../constants';
 
-const loopExplode = () => fetch(`${SERVER_URL}/loop/explode`, { method: 'POST'})
-
 function Select() {
-  return (
-    <div className="Select">
-      <input type="button" value="Loop explode" onClick={loopExplode}/>
-    </div>
-  );
+
+    const [lightProgrammes, setLightProgrammes] = useState([]);
+    useEffect(() => {
+        const request = fetch(`${SERVER_URL}/light_programmes`);
+        request.then(
+            result => result.json().then(
+                fetchedLightProgrammes => {
+                    setLightProgrammes(fetchedLightProgrammes)
+                    setSelectedLightProgramme(fetchedLightProgrammes[0])
+                }
+            )
+        )
+    }, []);
+
+    const [selectedLightProgramme, setSelectedLightProgramme] = useState(null);
+    const handleChange = (event: any) => setSelectedLightProgramme(event.target.value);
+    const loopLightProgramme = () => fetch(`${SERVER_URL}/loop/${selectedLightProgramme}`, { method: 'POST' });
+
+    return (
+        <div className="Select">
+            <select name="cars" id="cars" onChange={handleChange}>
+                {lightProgrammes.map(lightProgramme => <option value={lightProgramme}>{lightProgramme}</option>)}
+            </select>
+            <input type="button" value="Loop" onClick={loopLightProgramme} />
+        </div>
+    );
 }
 
 export default Select;
