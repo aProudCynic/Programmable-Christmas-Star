2047890ad6164e8364a9f38497a56cbb16977a26
Balazs Marton <balazs.marton@prezi.com>
2022-01-12 02:55:10 +0100

    fix: customise shutdown for blinkt to turn off leds

--- server/controller/blinkt/blinkt_controller.py
+++ server/controller/blinkt/blinkt_controller.py
@@ -2,6 +2,7 @@ from inspect import getmembers, isfunction
 
 from controller.light_controller import LightController
 import controller.blinkt.light_programmes as light_programmes
+from controller.blinkt.common import shut_down_all_pixels
 
 
 class BlinktController(LightController):
@@ -13,6 +14,10 @@ class BlinktController(LightController):
 
     def apply_light_programme(self, function):
         function()
+
+    def stop_loop(self):
+        super(BlinktController, self).stop_loop()
+        shut_down_all_pixels()
 
     # TODO hoist
     def get_light_programme_by(self, light_programme_name):
