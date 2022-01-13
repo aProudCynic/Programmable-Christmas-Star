103451d553d05ee38c68e8e3b2d39e2d8fda82cf
Balazs Marton <balazs.marton@prezi.com>
2022-01-13 01:47:25 +0100

    feat: add walk through pixels

--- server/controller/blinkt/light_programmes.py
+++ server/controller/blinkt/light_programmes.py
@@ -3,9 +3,17 @@ from time import sleep
 from controller.blinkt.common import (
     set_all_pixels_to_random_color,
     show,
+    walk_through_pixels_with,
+)
+
+from controller.blinkt.constants import (
+    RED,
 )
 
 def blink_random_colour():
     set_all_pixels_to_random_color()
     show()
     sleep(0.05)
+
+def walk_through_pixels():
+    walk_through_pixels_with(RED)
