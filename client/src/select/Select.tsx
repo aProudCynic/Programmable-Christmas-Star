fa7175d60f2188e45f66c2df14c16aa197abb2f8
Balazs Marton <balazs.marton@prezi.com>
2022-01-15 13:35:49 +0100

    feat: add dynamic client input generation
    client creates appropriate input for parameter type passed by server
    params are stored in state

--- client/src/select/Select.tsx
+++ client/src/select/Select.tsx
@@ -2,9 +2,37 @@ import React, { useContext, useEffect, useState } from 'react';
 import { SERVER_URL } from '../constants';
 import StartedContext from '../store/started-context';
 
+class Colour {
+    red: number;
+    green: number;
+    blue: number;
+
+    constructor(hexCode?: string) {
+        if(!hexCode) {
+            this.red = 0;
+            this.green = 0;
+            this.blue = 0;  
+        } else {
+            if(!hexCode.match(/#[a-f0-9]{6}/)) {
+                throw new Error(`Hex code is invalid: ${hexCode}`)
+            }
+            this.red = parseInt(hexCode.slice(1, 3), 16);
+            this.green = parseInt(hexCode.slice(3, 5), 16);
+            this.blue = parseInt(hexCode.slice(5, 7), 16);
+        }
+    }
+}
+
+
+
 interface LightProgramme {
     name: string;
-    parameters: string[];
+    parameters: ParameterDescriptor[];
+}
+
+interface ParameterDescriptor {
+    name: string;
+    type: string;
 }
 
 function Select() {
@@ -13,7 +41,8 @@ function Select() {
 
     const [lightProgrammes, setLightProgrammes] = useState<LightProgramme[]>([]);    
     const [selectedLightProgramme, setSelectedLightProgramme] = useState<LightProgramme | undefined>(undefined);
-    
+    const [parameters, setParameters] = useState<Record<string, string | number | Colour> | undefined>(undefined);
+
     useEffect(() => {
         const request = fetch(`${SERVER_URL}/light_programmes`);
         request.then(
@@ -22,15 +51,33 @@ function Select() {
                     fetchedLightProgrammes.forEach((element: any[]) => {
                         console.log(element)
                     });
-                    setLightProgrammes(fetchedLightProgrammes)
-                    setSelectedLightProgramme(fetchedLightProgrammes[0])
+                    setLightProgrammes(fetchedLightProgrammes);
+                    const selectedProgramme = fetchedLightProgrammes[0];
+                    setSelectedLightProgramme(selectedProgramme);
+                    resetParametersByDefaultsFor(selectedProgramme.parameters);
                 }
             )
         )
     }, []);
 
-    const [selectedLightProgramme, setSelectedLightProgramme] = useState<LightProgramme | undefined>(undefined);
-    const handleChange = (event: any) => setSelectedLightProgramme(lightProgrammes.find(lightProgramme => lightProgramme.name === event.target.value));
+    const getDefaultValueFor = (type: string) => {
+        switch(type) {
+            case 'str':
+                return '';
+            case 'int':
+                return 0;
+            case 'Colour':
+                return new Colour('#000000');
+        }
+    }
+
+    const handleChange = (event: any) => {
+        const selectedProgramme = lightProgrammes.find(lightProgramme => lightProgramme.name === event.target.value);
+        setSelectedLightProgramme(selectedProgramme);
+        if (selectedProgramme) {
+            resetParametersByDefaultsFor(selectedProgramme);
+        };
+    }
     
     const loopLightProgramme = async () => {
         if (selectedLightProgramme) {
@@ -41,13 +88,40 @@ function Select() {
         }
     };
 
+    const handleColorParameterChange = (event: any) => {
+        const newColour = new Colour(event.target.value);
+        const parameterName = event.target.name;
+        const newParameters = {...parameters};
+        newParameters[parameterName] = newColour;
+        console.log(newParameters);
+        setParameters(newParameters);
+    }
+
+    function resetParametersByDefaultsFor(programme: LightProgramme) {
+        const newParameters: any = {};
+        programme.parameters.forEach(
+            (parameterDescriptor: ParameterDescriptor) => newParameters[parameterDescriptor.name] = getDefaultValueFor(parameterDescriptor.type)
+        );
+        setParameters(newParameters);
+    }
+
+    const mapParameterDescriptorToHtmlElement = (parameter: ParameterDescriptor) => {
+        switch(parameter.type) {
+            case 'str':
+                return <p><label htmlFor={parameter.name}>{parameter.name}</label> <input type="text" id={parameter.name} name={parameter.name}></input></p>
+            case 'Colour':
+                return <p><label htmlFor={parameter.name}>{parameter.name}</label> <input type="color" id={parameter.name} name={parameter.name} onChange={handleColorParameterChange}></input></p>
+        }
+        
+    }
+
     return (
         <div className="Select">
             <select name="ligthProgramme" id="ligthProgramme" onChange={handleChange} disabled={startedContext.isStarted}>
                 {lightProgrammes.map(lightProgramme => <option value={lightProgramme.name}>{lightProgramme.name}</option>)}
             </select>
             { selectedLightProgramme && selectedLightProgramme.parameters ? selectedLightProgramme.parameters.map(
-                parameter => <p><label htmlFor={parameter}>{parameter}</label> <input type="text" id={parameter} name={parameter} defaultValue="0"></input></p>
+                parameter => mapParameterDescriptorToHtmlElement(parameter)
             ) : null }
             <input type="button" value="Loop" onClick={loopLightProgramme} disabled={startedContext.isStarted}/>
         </div>
