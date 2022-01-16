1f39450686fffa3ad348d2cf01dde346e18ce902
Balazs Marton <balazs.marton@prezi.com>
2022-01-16 14:21:32 +0100

    feat: implement timed auto-shutdown for loop

--- server/controller/light_controller.py
+++ server/controller/light_controller.py
@@ -1,6 +1,7 @@
 from inspect import getmembers, isfunction
 from multiprocessing import Process
-from typing import List
+from threading import Thread
+from time import sleep
 
 
 class LightController:
@@ -8,6 +9,13 @@ class LightController:
     def start_loop(self, function, parameters) -> None:
         self.process = Process(target=self.perform_loop, args=(function, parameters))
         self.process.start()
+
+    def start_loop_with_ttl(self, function, parameters) -> None:
+        self.start_loop(function, parameters)
+        time_to_live_secs = 10
+        if time_to_live_secs > 0:
+            self.countdown = Thread(target=self.perform_countdown, args=(time_to_live_secs,))
+            self.countdown.start()
 
     def perform_loop(self, function, parameters) -> None:
         while True:
@@ -25,7 +33,7 @@ class LightController:
     def get_light_programme_by(self, light_programme_name):
         return getattr(self.get_light_programme_module(), light_programme_name)
 
-    def get_light_programme_module():
+    def get_light_programme_module(self):
         pass
 
     def apply_light_programme(self, function, parameters) -> None:
@@ -33,6 +41,10 @@ class LightController:
 
     def is_on(self):
         return hasattr(self, 'process') and self.process and self.process.is_alive()
+
+    def perform_countdown(self, seconds):
+        sleep(seconds)
+        self.stop_loop()
 
     def __not_imported_functions_from(self, module):
         function_tuples = getmembers(module, isfunction)
